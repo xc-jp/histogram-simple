5bb71381c965f573acad6324b6e9cfaa9fc52fa4
Jonas Carpay <jonascarpay@gmail.com>
2021-02-03 09:33:53 +0900

    fix tooling

--- shell.nix
+++ shell.nix
@@ -1,19 +1,14 @@
 let
   pkgs = import ./pkgs.nix;
   hsPkgs = pkgs.hsPkgs;
-  ormolu = pkgs.haskell-nix.tool hsPkgs.projectArgs.compiler-nix-name "ormolu" "latest";
-  # Warning: only works with ghc 8.10 and up!
-  ormolu-wrapped = pkgs.writeShellScriptBin "ormolu" ''
-    ${ormolu}/bin/ormolu --ghc-opt=-XImportQualifiedPost $@
-  '';
 in
 hsPkgs.shellFor {
   withHoogle = true;
   tools = {
     cabal = "latest";
     ghcid = "latest";
+    ormolu = "latest";
     haskell-language-server = "latest";
   };
-  buildInputs = [ ormolu-wrapped ];
   exactDeps = true;
 }
